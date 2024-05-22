7589691582b46d7ac0fc3867dc0f10c30c7edc87
diegougu <128000056+diegougu@users.noreply.github.com>
2024-05-21 23:52:37 -0500

    Update Imprimirarbollinealvector.cpp

--- Imprimirarbollinealvector.cpp
+++ Imprimirarbollinealvector.cpp
@@ -30,7 +30,7 @@ private:
     bool Find(int x, CBinNode**& p);
     CBinNode** Rep(CBinNode** p);
     void InOrder(CBinNode* n);
-    void Filas(vector<int>& valores, CBinNode* ptr, CBinNode* ptr2);
+    void Filas(vector<int>& valores, CBinNode* ptr, CBinNode* ptr2, CBinNode* ptr3, CBinNode* ptr4);
 
     CBinNode* m_root;
     bool m_b;
@@ -102,45 +102,46 @@ void CBinTree::Print()
 }
 
 
-void CBinTree::Filas(vector<int>& valores, CBinNode* ptr, CBinNode* ptr2) {
+void CBinTree::Filas(vector<int>& valores, CBinNode* ptr, CBinNode* ptr2, CBinNode* ptr3, CBinNode* ptr4) {
     if (!ptr && !ptr2) return;
 
     if (ptr == m_root && ptr2 == m_root) {
         valores.push_back(ptr->value);
-        Filas(valores, ptr->nodes[0], ptr2->nodes[1]);
+        Filas(valores, ptr->nodes[0], ptr2->nodes[1], ptr3, ptr4);
         ptr = ptr->nodes[0];
         ptr2 = ptr2->nodes[1];
     }
-    else if (ptr == nullptr && ptr2 != nullptr) {
-        ptr = ptr2;
-        valores.push_back(ptr2->value);
-        Filas(valores, ptr->nodes[0], ptr->nodes[1]);
-    }
-    else if (ptr != nullptr && ptr2 == nullptr) {
-        ptr2 = ptr;
-        valores.push_back(ptr->value);
-        Filas(valores, ptr->nodes[0], ptr->nodes[1]);
-    }
-
 
     else {
         if (ptr != nullptr) {
             valores.push_back(ptr->value);
         }
+        
+        if(ptr3 != nullptr){
+            valores.push_back(ptr3->value);
+        }
+        
         if (ptr2 != nullptr) {
             valores.push_back(ptr2->value);
         }
-        Filas(valores, ptr->nodes[0], ptr->nodes[1]);
-        Filas(valores, ptr2->nodes[0], ptr2->nodes[1]);
-    }
+        
+        if(ptr4 != nullptr){
+            valores.push_back(ptr4->value);
 
+        }
+        
+        
+        Filas(valores, ptr->nodes[0], ptr2->nodes[0], ptr->nodes[1], ptr2->nodes[1]);
+    }
 }
 
 
 
 void CBinTree::Print2(vector<int> valores) {
+    CBinNode* ptr3 = nullptr;
+    CBinNode* ptr4 = nullptr;
 
-    Filas(valores, m_root, m_root);
+    Filas(valores, m_root, m_root, ptr3, ptr4);
 
 
     for (size_t i = 0; i < valores.size(); ++i) {
@@ -154,11 +155,10 @@ int main()
     CBinTree t;
     t.Insert(55);
     t.Insert(35);
-    t.Insert(41);
-    t.Insert(20);
     t.Insert(77);
-    t.Insert(61);
-    t.Insert(92);
+    t.Insert(20);
+    t.Insert(1);
+    t.Insert(200);
     t.Print();
 
     vector<int> val;
